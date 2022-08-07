46ab11c91210f013dba48900b20526de65a1dffb
ibrahim keskin <ibrahim.keskin@mlpcare.com>
2022-08-07 14:48:12 +0300

    Create express api

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require("express")
+
+const app=express()
+
+const PORT=5000||process.env.PORT;
+
+app.get("/",(req,res,next)=>{
+    res.send("Hello Question Answer Api");
+    next();
+});
+app.listen(PORT,()=>{
+    console.log(`App Started on ${PORT}`);
+})
