49e30e3bf1762679defc5233d7a214c39df6728f
ibrahim keskin <ibrahim.keskin@mlpcare.com>
2022-08-07 14:59:02 +0300

    env ve config ayarları

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express=require("express")
+const dotenv=require("dotenv");
 
 const app=express()
 
-const PORT=5000||process.env.PORT;
+// Enviroment Variables
+
+dotenv.config({
+    path:"./config/env/config.env"
+});
+
+const PORT=process.env.PORT;
+
+
+
 
 app.get("/",(req,res,next)=>{
     res.send("Hello Question Answer Api - ibrahim");
     next();
 });
 app.listen(PORT,()=>{
-    console.log(`App Started on ${PORT}`);
+    console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`);
 })
