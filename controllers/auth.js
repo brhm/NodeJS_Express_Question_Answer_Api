ec8198171fd3f9388226b91c32c37f3519bffce7
ibrahim keskin <ibrahim.keskin@mlpcare.com>
2022-08-13 13:41:27 +0300

    update auth register post

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,24 @@
-const register=(req,res,next)=>{
+const User=require("../models/User");
+
+const register=async (req,res,next)=>{
+    // post data 
+
+    const name="medya keskin";
+    const email="medyaydin@gmail.com";
+    const password="123qwe";
+
+    // async ,await 
+
+    const user= await User.create({
+        name,
+        email,
+        password
+    });
 
     res.status(200)
     .json({
         success:true,
-        body:"Auth register page"
+        data:user
     })
 };
 
