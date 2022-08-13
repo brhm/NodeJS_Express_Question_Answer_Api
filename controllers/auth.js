5794a19ab1743dfc0c00044e9bd0895b427dfbd8
ibrahim keskin <ibrahim.keskin@mlpcare.com>
2022-08-13 16:41:20 +0300

    JWT auhorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const User=require("../models/User");
 const CustomError=require("../helpers/error/CustomError");
-const asyncErrorWrapper=require("express-async-handler"); // bu asynchandler sayesinde try catch leri kullanmadan hataları Custom Error Handlera yönlendirilmesini sağlıyoruz.
 const { JsonWebTokenError } = require("jsonwebtoken");
+const sendJwtToClient=require("../helpers/authorization/sendJwtToClient");
+const asyncErrorWrapper=require("express-async-handler"); // bu asynchandler sayesinde try catch leri kullanmadan hataları Custom Error Handlera yönlendirilmesini sağlıyoruz.
+
 
 const register= asyncErrorWrapper (async(req,res,next)=>{
     //Post Data
@@ -16,14 +18,7 @@ const register= asyncErrorWrapper (async(req,res,next)=>{
         password,
         role
     });
-
-    const token=user.generateJwtFromUser();
-    console.log(token);
-    res.status(200)
-    .json({
-        success:true,
-        data:user
-    })
+sendJwtToClient(user,res);
 
 });
 
