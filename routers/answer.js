137db91396ce2c4e38f4ea5b849c897432597da6
ibrahim keskin <ibrahim.keskin@mlpcare.com>
2022-08-21 15:21:47 +0300

    like and undolike to answer

--- routers/answer.js
+++ routers/answer.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const {getAccessToRoute}=require("../middleware/authorization/auth");
-const {addNewAnswerToQuestion,getAllAnswersByQuestion,getSingleAnswer,editAnswwer,deleteAnswwer}=require("../controllers/answer");
+const {addNewAnswerToQuestion,getAllAnswersByQuestion,getSingleAnswer,editAnswwer,deleteAnswwer,likeAnswer,undoLikeAnswer}=require("../controllers/answer");
 const {checkQuestionAndAnswerExist} =require("../middleware/database/databaseErrorHelpers");
 const {getAnswerOwnerAccess}=require("../middleware/authorization/auth");
 // mergeParams => bir üst route taki paramsları alt router geçmesini sağlıyor
@@ -10,6 +10,8 @@ const router=express.Router({mergeParams:true});
 router.post("/",getAccessToRoute,addNewAnswerToQuestion);
 router.get("/",getAccessToRoute,getAllAnswersByQuestion);
 router.get("/:answer_id",checkQuestionAndAnswerExist,getSingleAnswer)
+router.get("/:answer_id/like",[checkQuestionAndAnswerExist,getAccessToRoute],likeAnswer)
+router.get("/:answer_id/undolike",[checkQuestionAndAnswerExist,getAccessToRoute],undoLikeAnswer)
 router.put("/:answer_id/edit",[checkQuestionAndAnswerExist,getAccessToRoute,getAnswerOwnerAccess],editAnswwer)
 router.delete("/:answer_id/delete",[checkQuestionAndAnswerExist,getAccessToRoute,getAnswerOwnerAccess],deleteAnswwer)
 
